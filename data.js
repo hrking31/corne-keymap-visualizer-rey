c1648a803de03e49f34e3ce6f265bf27d640a1b1
Hernando Rey <hrking31@gmail.com>
2026-02-11 15:02:22 -0500

    se mejoran estilos como mas espacio entre teclas en móviles y se centran mejor los teclados en móviles , se agregan nuevas letras y capas al keymap

--- data.js
+++ data.js
@@ -1,33 +1,34 @@
 const keymap = {
+  // ---------------- CAPA BASE -----------------
   BASE: [
-    // --- BLOQUE DERECHO (Físicamente en left-side, index 0-20) ---
+    // --- BLOQUE DERECHO ---
     // Fila 1
-    { label: "ESC", desc: "Key   0" },
-    { label: ".", desc: "Key   1", extra: ":" },
-    { label: ",", desc: "Key   2", extra: ";" },
-    { label: "Ñ", desc: "Key   3", extra: "Alt Gr + N" },
-    { label: "P", desc: "Key   4" },
-    { label: "Y", desc: "Key   5" },
+    { label: "ESC", desc: "Key 0" },
+    { label: ".", desc: "Key 1", extra: "Con SHIFT = :" },
+    { label: ",", desc: "Key 2", extra: "Con SHIFT = ;" },
+    { label: "Ñ", desc: "Key 3" },
+    { label: "P", desc: "Key 4" },
+    { label: "Y", desc: "Key 5" },
     // Fila 2
-    { label: "ALT ", desc: "Key   12" },
-    { label: "A", desc: "Key   13" },
-    { label: "O", desc: "Key   14" },
-    { label: "E", desc: "Key   15" },
-    { label: "U", desc: "Key   16" },
-    { label: "I", desc: "Key   17" },
+    { label: "ALT ", desc: "Key 12" },
+    { label: "A", desc: "Key 13" },
+    { label: "O", desc: "Key 14" },
+    { label: "E", desc: "Key 15" },
+    { label: "U", desc: "Key 16" },
+    { label: "I", desc: "Key 17" },
     // Fila 3
-    { label: "CTRL", desc: "Key  24" },
-    { label: "", desc: "Key  25", extra: "Nada Asignado" },
-    { label: "Q", desc: "Key   26" },
-    { label: "J", desc: "Key   27" },
+    { label: "CTRL", desc: "Key 24" },
+    { label: "´", desc: "Key 25", extra: "Tilde" },
+    { label: "Q", desc: "Key 26" },
+    { label: "J", desc: "Key 27" },
     { label: "K", desc: "Key 28" },
     { label: "X", desc: "Key 29" },
     // Pulgares
-    { label: "SHFT", desc: "Key 36" },
-    { label: "NUM", desc: "Key 37" },
+    { label: "SHIFT", desc: "Key 36" },
+    { label: "NUM", desc: "Key 37", extra: "Cambia a la capa NUM (números)" },
     { label: "SPC", desc: "Key 38" },
 
-    // --- BLOQUE IZQUIERDO (Físicamente en right-side, index 21-41) ---
+    // --- BLOQUE IZQUIERDO ---
     // Fila 1
     { label: "F", desc: "Key 6" },
     { label: "G", desc: "Key 7" },
@@ -51,39 +52,69 @@ const keymap = {
     { label: "TAB", desc: "Key 35" },
     // Pulgares
     { label: "ENT", desc: "Key 39" },
-    { label: "SIN", desc: "Key 40" },
-    { label: "GUI", desc: "Key 41" },
+    { label: "SIN", desc: "Key 40", extra: "Cambia a la capa SYM (símbolos)" },
+    { label: "WIN", desc: "Key 41" },
   ],
 
+  // ---------------- CAPA NÚMEROS -----------------
   NUM: [
-    // --- BLOQUE DERECHO (Físicamente en left-side, index 0-20) ---
+    // --- BLOQUE DERECHO ---
     // Fila 1
     { label: "ESC", desc: "Key 0" },
-    { label: "P", desc: "Key 1" },
-    { label: "Ñ", desc: "Key 2" },
-    { label: ",", desc: "Key 3" },
-    { label: ".", desc: "Key 4" },
-    { label: "ESC", desc: "Key 5" },
+    { label: "1", desc: "Key 1" },
+    { label: "2", desc: "Key 2" },
+    { label: "3", desc: "Key 3" },
+    { label: "4", desc: "Key 4" },
+    { label: "5", desc: "Key 5" },
     // Fila 2
     { label: "ALT", desc: "Key 12" },
-    { label: "Key16", desc: "Key 13" },
-    { label: "Key15", desc: "Key 14" },
-    { label: "Key14", desc: "Key 15" },
-    { label: "Key13", desc: "Key 16" },
-    { label: "Key12", desc: "Key 17" },
+    { label: "FOCO", desc: "Key 13", extra: "Foco en archivo de VSC" },
+    { label: "FLDR", desc: "Key 14", extra: "Abrir nueva carpeta en VSC" },
+    {
+      label: "INI",
+      desc: "Key 15",
+      extra: "Me lleva al inicio de la linea en VSC",
+    },
+    {
+      label: "C-UP",
+      desc: "Key 16",
+      extra: "Mueve la linea actual hacia arriba en VSC",
+    },
+    {
+      label: "UNDO",
+      desc: "Key 16",
+      extra: "Deshace la última acción en VSC",
+    },
     // Fila 3
     { label: "CTRL", desc: "Key 24" },
-    { label: "Key28", desc: "Key 25" },
-    { label: "Key27", desc: "Key 26" },
-    { label: "Key26", desc: "Key 27" },
-    { label: "Key25", desc: "Key 28" },
-    { label: "Key24", desc: "Key 29" },
+    { label: "", desc: "Key 25" },
+    { label: "FILE", desc: "Key 26", extra: "Abre nuevo archivo en VSC" },
+    {
+      label: "END",
+      desc: "Key 27",
+      extra: "Me lleva al final de la linea en VSC",
+    },
+    {
+      label: "C-DN",
+      desc: "Key 28",
+      extra: "Mueve la linea actual hacia abajo en VSC",
+    },
+    {
+      label: "REDO",
+      desc: "Key 29",
+      extra: "Rehace la última acción en VSC",
+    },
     // Pulgares
-    { label: "SHFT", desc: "Key 36" },
-    { label: "NUM", desc: "Key 37" },
+    { label: "SHIFT", desc: "Key 36" },
+    {
+      label: "NUM",
+      desc: "Key 37",
+      extra:
+        "Al presionar y mantener junto con NAV cambia a la capa NAV (navegación)",
+    },
     { label: "SPC", desc: "Key 38" },
 
-    // --- BLOQUE IZQUIERDO (Físicamente en right-side, index 21-41) ---
+    // --- BLOQUE IZQUIERDO ---
     // Fila 1
     { label: "Key6", desc: "Key 6" },
     { label: "Key7", desc: "Key 7" },
@@ -107,12 +138,23 @@ const keymap = {
     { label: "TAB", desc: "Key 35" },
     // Pulgares
     { label: "ENT", desc: "Key 39" },
-    { label: "NAV", desc: "Key 40" },
-    { label: "FUN", desc: "Key 41" },
+    {
+      label: "NAV",
+      desc: "Key 40",
+      extra:
+        "Cambia a la capa NAV (navegación), debe de estar en capa NUM para funcionar",
+    },
+    {
+      label: "FUN",
+      desc: "Key 41",
+      extra:
+        "Cambia a la capa FUN (funciones), debe de estar en capa NUM para funcionar",
+    },
   ],
 
-    SYM: [
-    // --- BLOQUE DERECHO (Físicamente en left-side, index 0-20) ---
+  // ---------------- CAPA SÍMBOLOS -----------------
+  SYM: [
+    // --- BLOQUE DERECHO ---
     // Fila 1
     { label: "ESC", desc: "Key 0" },
     { label: "P", desc: "Key 1" },
@@ -135,11 +177,11 @@ const keymap = {
     { label: "Key25", desc: "Key 28" },
     { label: "Key24", desc: "Key 29" },
     // Pulgares
-    { label: "SHFT", desc: "Key 36" },
+    { label: "SHIFT", desc: "Key 36" },
     { label: "LED", desc: "Key 37" },
     { label: "SPC", desc: "Key 38" },
 
-    // --- BLOQUE IZQUIERDO (Físicamente en right-side, index 21-41) ---
+    // --- BLOQUE IZQUIERDO ---
     // Fila 1
     { label: "Key6", desc: "Key 6" },
     { label: "Key7", desc: "Key 7" },
@@ -164,6 +206,6 @@ const keymap = {
     // Pulgares
     { label: "ENT", desc: "Key 39" },
     { label: "SYM", desc: "Key 40" },
-    { label: "", desc: "Key 41" },
+    { label: "WIN", desc: "Key 41" },
   ],
 };
